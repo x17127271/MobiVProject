e4bd8aab047e0016250bb823f3b1cde0eba1e100
zjameszhou <57330161+zjameszhou@users.noreply.github.com>
2020-04-06 11:38:27 +0100

    about page

--- MobiV/config/environments/production.rb
+++ MobiV/config/environments/production.rb
@@ -25,8 +25,10 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
+  config.serve_static_assets = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
